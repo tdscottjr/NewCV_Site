28dad672eb21fc68b61d6d2cc4860f7f0ff98485
Tyrone Scott <tdscottjr@gmail.com>
2023-01-05 16:44:07 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+const firstname = document.getElementById('first_name')
+const lastname = document.getElementById('last_name')
+const email = document.getElementById('e_mail')
+const contact_form = document.getElementById('contact_form')
